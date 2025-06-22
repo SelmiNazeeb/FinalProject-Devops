cd22d0e5d34299436140c79f2d4fe039df2b2c46
root <root@ip-172-31-90-135.ec2.internal>
2025-06-22 11:35:24 +0000

    buil-terra

--- terraform_infra/main.tf
+++ terraform_infra/main.tf
@@ -337,19 +337,4 @@ module "eks" {
 # Outputs
 #############################
 
-output "vpc_id" {
-  value = aws_vpc.this.id
-}
-
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "eks_cluster_security_group_id" {
-  value = aws_security_group.eks_cluster.id
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.this.endpoint
-}
 
--- terraform_infra/output.tf
+++ terraform_infra/output.tf
@@ -8,6 +8,10 @@ output "eks_cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
+output "eks_cluster_security_group_id" {
+  description = "eks cluster security group" 
+  value = aws_security_group.eks_cluster.id
+}
 output "rds_endpoint" {
   description = "RDS instance endpoint"
   value       = aws_db_instance.this.endpoint
