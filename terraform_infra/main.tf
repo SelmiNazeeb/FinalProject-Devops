b7b9ed9852d76b826c0f3be4ee4e16337f296ddb
root <root@ip-172-31-90-135.ec2.internal>
2025-06-23 03:21:36 +0000

    add

--- terraform_infra/main.tf
+++ terraform_infra/main.tf
@@ -328,7 +328,7 @@ module "eks" {
   subnet_ids  = aws_subnet.private[*].id
 
   cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = false
+  cluster_endpoint_public_access  = true
 
   cluster_security_group_id      = aws_security_group.eks_cluster.id
   create_node_security_group     = false  # <- tells the module to not create its own
