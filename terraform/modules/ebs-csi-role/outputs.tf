7a26501af1a473a187ae9b5eeab6ab6820c2b2a4
root <root@ip-172-31-90-135.ec2.internal>
2025-06-23 11:52:47 +0000

    add

--- terraform/modules/ebs-csi-role/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "EBS CSI driver role ARN"
-  value       = aws_iam_role.ebs_csi.arn
-}
-
-output "name" {
-  description = "EBS CSI driver role name"
-  value       = aws_iam_role.ebs_csi.name
-} 
--- terraform/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "CodeBuild role ARN"
-  value       = aws_iam_role.codebuild.arn
-}
-
-output "name" {
-  description = "CodeBuild role name"
-  value       = aws_iam_role.codebuild.name
-} 
